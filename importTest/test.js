89c8190c658c721b20a1d4ca2069273c1b9b06a5
e1182086-sanuk_sangroup <jose.marinarmenteros@santander.co.uk>
2023-11-13 19:36:25 +0000

    example with chrono

--- importTest/test.js
+++ importTest/test.js
@@ -1,3 +1,17 @@
-import { innerRightJoinWith, firstCapital, plan, Chrono } from 'js-awe'
+import {Chrono, sleep } from 'js-awe'
 
-console.log(firstCapital('test'))
+let chrono = Chrono()
+
+chrono.time('step0')
+await sleep(50)
+chrono.timeEnd('step0')
+chrono.time('step1')
+chrono.time('step2')
+await Promise.all([
+  sleep(150).then(() => chrono.timeEnd('step1')),
+  sleep(100).then(() => chrono.timeEnd('step2'))
+])
+chrono.time('step3')
+await sleep(50)
+chrono.timeEnd('step3')
+chrono.report()
